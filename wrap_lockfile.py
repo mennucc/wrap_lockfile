117c4a0e69bbd8ecba022e29911f1549d236727b
Andrea C. G. Mennucci <mennucc1@debian.org>
2025-11-10 19:57:25 +0100

    in `atomic_write_no_lock` ,  check `mode`

--- wrap_lockfile.py
+++ wrap_lockfile.py
@@ -389,6 +389,7 @@ class atomic_write_no_lock(object):
                 Same parameters as built-in open() function
         """
         self.filename = os.path.abspath(filename)
+        assert ( isinstance(mode,str) and re.fullmatch(r'[rwa](\+)?b?', mode) ) , f"Invalid file mode: {mode!r}"
         self.mode = mode
         self.buffering = buffering
         self.encoding = encoding
